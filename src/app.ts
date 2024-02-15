eda20f43b9cf1b7c5ca08347a0ecba2df820f8ac
Felipe <felipe.kobi@hotmail.comgit commit -mprimeira>
2024-02-15 11:48:35 -0300

    add: functions

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,16 @@ import { handleAppErrorMiddlieware } from "./middlewares/handleAppError.middlewa
 import { sessionRouter } from "./routes/session.route";
 import { contactRouter } from "./routes/contact.routes";
 import { passwordRouter } from "./routes/password.routes";
-
+import cors from "cors"
 
 
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 app.use("/users", userRouter)
 app.use("/login", sessionRouter)
 app.use("/contacts", contactRouter)
